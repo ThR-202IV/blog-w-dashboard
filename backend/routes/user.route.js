0f1c85f043a6d47a81b3366dd7c985b7424ba0bf
Thameem <thameemzakeen.official@gmail.com>
2024-03-19 17:34:14 +0530

    added update user functionality in api

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.put("/update/:userId", verifyToken, updateUser);
 
 /* 'cause we export router as a default, we are able to change its name in the component we import this, for example, in index.js, we imoprt router as userRoutes */
 export default router;
