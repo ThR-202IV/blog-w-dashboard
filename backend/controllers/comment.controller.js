b4ce6f167a33182bd1b9a0df229db53c47a0b592
Thameem <thameemzakeen.official@gmail.com>
2024-03-30 16:52:09 +0530

    added edit functionality to the comment component

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -61,3 +61,30 @@ export const likeComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(404, "You're not allowed to edit this comment!")
+      );
+    }
+
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      /* we're changing only the content */
+      {
+        content: req.body.content,
+      },
+      /* we want the latest updated comment returned */
+      { new: true }
+    );
+    res.status(200).json(editedComment);
+  } catch (error) {}
+};
--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from "react";
 import moment from "moment";
 import { IoMdThumbsUp } from "react-icons/io";
 import { useSelector } from "react-redux";
+import { Button, Textarea } from "flowbite-react";
 
-const Comment = ({ comment, onLike }) => {
+const Comment = ({ comment, onLike, onEdit }) => {
   const { currentUser } = useSelector((state) => state.user);
   const [user, setUser] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedComment, setEditedComment] = useState(comment.content);
 
   useEffect(() => {
     const getUser = async () => {
@@ -23,6 +26,32 @@ const Comment = ({ comment, onLike }) => {
     };
     getUser();
   }, [comment]);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedComment(comment.content);
+  };
+
+  const handleSave = async () => {
+    try {
+      const resp = await fetch(`/api/comment/editComment/${comment._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: editedComment,
+        }),
+      });
+
+      if (resp.ok) {
+        setIsEditing(false);
+        onEdit(comment, editedComment);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
@@ -42,30 +71,71 @@ const Comment = ({ comment, onLike }) => {
             {moment(comment.createdAt).fromNow()}
           </span>
         </div>
-        <p className="text-gray-500 dark:text-gray-200 pb-2">
-          {comment.content}
-        </p>
-        {/* max-w-fit is the reason why the border above the thumbs-up icon and the number of likes fits the size of it  */}
-        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
-          <button
-            type="button"
-            onClick={() => onLike(comment._id)}
-            /* dynamically changes tailwind styles based on a condition */
-            className={`text-gray-300 dark:text-gray-600 hover:text-green-500 dark:hover:text-green-300 ${
-              currentUser &&
-              comment.likes.includes(currentUser._id) &&
-              "!text-green-500 dark:!text-green-300"
-            }`}
-          >
-            <IoMdThumbsUp className="text-sm" />
-          </button>
-          <p className="text-gray-400">
-            {comment.numberOfLikes > 0 &&
-              comment.numberOfLikes +
-                " " +
-                (comment.numberOfLikes === 1 ? "like" : "likes")}
-          </p>
-        </div>
+        {isEditing ? (
+          <>
+            <Textarea
+              value={editedComment}
+              onChange={(e) => setEditedComment(e.target.value)}
+              className="mb-2"
+            />
+            <div className="flex justify-end gap-2 text-xs">
+              <Button
+                type="button"
+                size="sm"
+                gradientMonochrome="teal"
+                onClick={handleSave}
+              >
+                Save
+              </Button>
+              <Button
+                type="button"
+                size="sm"
+                gradientMonochrome="teal"
+                outline
+                onClick={() => setIsEditing(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="text-gray-500 dark:text-gray-200 pb-2">
+              {comment.content}
+            </p>
+            {/* max-w-fit is the reason why the border above the thumbs-up icon and the number of likes exactly fits its size */}
+            <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+              <button
+                type="button"
+                onClick={() => onLike(comment._id)}
+                /* dynamically changes tailwind styles based on a condition */
+                className={`text-gray-300 dark:text-gray-600 hover:text-green-500 dark:hover:text-green-300 ${
+                  currentUser &&
+                  comment.likes.includes(currentUser._id) &&
+                  "!text-green-500 dark:!text-green-300"
+                }`}
+              >
+                <IoMdThumbsUp className="text-sm" />
+              </button>
+              <p className="text-gray-400">
+                {comment.numberOfLikes > 0 &&
+                  comment.numberOfLikes +
+                    " " +
+                    (comment.numberOfLikes === 1 ? "like" : "likes")}
+              </p>
+              {currentUser &&
+                (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                  <button
+                    type="button"
+                    onClick={handleEdit}
+                    className="text-gray-400 dark:text-gray-200 hover:text-green-500 dark:hover:text-green-300"
+                  >
+                    Edit
+                  </button>
+                )}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
