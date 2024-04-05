b72e93ccebb025bdf3f58fca574abc1e9c6d8beb
Thameem <thameemzakeen.official@gmail.com>
2024-04-05 17:08:28 +0530

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -14,6 +15,9 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION)
   .then(() => console.log("Connection to the Database is successful! ——"))
   .catch((error) => console.log(`Error: ${error} ——`));
+
+/* __dirname gives the directory of the place where our project is available. Whilst in localhost it is the folder where it is stored but when it si uploaded to render, it will be different */
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -28,6 +32,13 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+/* static folder will represent our frontend side. If VITE is used (like in this project), but if 'npx create-react-app' is used it should be "/build". This statement will find the folder and run index.html */
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 /* this error is the error that comes from the input. The error passed by the next method that manages the errorHandler comes here */
 app.use((error, req, res, next) => {
